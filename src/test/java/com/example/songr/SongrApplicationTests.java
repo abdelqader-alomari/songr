9fc3c0a0150d433e0fa93807aa7d48302fb175ec
abdelqader-alomari <aboud.coding@gmail.com>
2021-10-10 21:27:18 +0300

    Lab 11

--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.songr;
 
+import com.example.songr.model.Album;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class SongrApplicationTests {
+class SongrApplicationTests<Albums> {
+	Album albums = new Album("MyWay", "Frank Sinatra", 10, 1947,"https://th.bing.com/th/id/R.30f669b2967e9ab95bf74268c52f0f8a?rik=my6BBb79N6n%2bLQ&pid=ImgRaw&r=0");
 
 	@Test
-	void contextLoads() {
+	public void constructorTest(){
+
+		Assertions.assertNotNull(albums);
+	}
+
+	@Test
+	public void setAndGetTest(){
+
+		Album MyWay = new Album("MyWay", "Frank Sinatra", 10, 1947,"https://th.bing.com/th/id/R.30f669b2967e9ab95bf74268c52f0f8a?rik=my6BBb79N6n%2bLQ&pid=ImgRaw&r=0");
+		assertEquals("Frank Sinatra", MyWay.getArtist());
+
+		MyWay.setLength(2000);
+		assertEquals(2000,MyWay.getLength());
 	}
 
 }
