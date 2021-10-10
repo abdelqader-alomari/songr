9897e2c5b633ab707abe619292f4a1d605c8b2c1
abdelqader-alomari <aboud.coding@gmail.com>
2021-10-10 23:34:38 +0300

    lab 11

--- src/main/java/com/example/songr/controller/HelloController.java
+++ src/main/java/com/example/songr/controller/HelloController.java
@@ -1,24 +1,25 @@
 package com.example.songr.controller;
 
 import com.example.songr.model.Album;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
 
-public class HelloController{
+@Controller
+public class HelloController {
     @GetMapping("/hello")
     public String greetings(@RequestParam(name = "name", required = false, defaultValue = "world") String name,
             Model model) {
         model.addAttribute("name", name); // this is passed to the template automatically
-        return "hello"; // this represents the name of the html template which will be rendered
+        return "hello.html"; // this represents the name of the html template which will be rendered
     }
 
     @GetMapping("/")
     public String homepage() {
-        return "home";
+        return "index";
     }
 
     @GetMapping("/capitalize/{hello}")
@@ -33,8 +34,7 @@ public class HelloController{
             new Album("L-O-V-E", "Nat King Cole", 16, 2558,
                     "https://th.bing.com/th/id/R.23144b7b337175c9b7b23c0ce35e2b32?rik=Ep0xStSXR3jxJw&riu=http%3a%2f%2fcps-static.rovicorp.com%2f3%2fJPG_400%2fMI0000%2f012%2fMI0000012214.jpg%3fpartner%3dallrovi.com&ehk=LczjV1TPUSSnn3bSm0ERWB0xlTjYucj7ts4deE16LLI%3d&risl=&pid=ImgRaw&r=0"),
             new Album("parachutes", "Coldplay", 10, 2489,
-                    "https://th.bing.com/th/id/R.7df771f6610835736b113a810e301ead?rik=FO0S7N69jpw6qQ&riu=http%3a%2f%2f3.bp.blogspot.com%2f-wJ1hDEcCWkA%2fUUYpYtbpvuI%2fAAAAAAAABxM%2fimRLmzbjRV4%2fs1600%2fcoldplay-parachutes_cd.jpg&ehk=lxmfBlkqGj9bBNNDZn7B1TM%2bQ2RAi5vVlNZ7hbn%2bt2M%3d&risl=&pid=ImgRaw&r=0")
-    };
+                    "https://th.bing.com/th/id/R.7df771f6610835736b113a810e301ead?rik=FO0S7N69jpw6qQ&riu=http%3a%2f%2f3.bp.blogspot.com%2f-wJ1hDEcCWkA%2fUUYpYtbpvuI%2fAAAAAAAABxM%2fimRLmzbjRV4%2fs1600%2fcoldplay-parachutes_cd.jpg&ehk=lxmfBlkqGj9bBNNDZn7B1TM%2bQ2RAi5vVlNZ7hbn%2bt2M%3d&risl=&pid=ImgRaw&r=0") };
 
     @GetMapping("/albums")
     public String albums(Model model) {
