6cadabcf998926d5b044a05a7e01eaaf3660ec2c
abdelqader-alomari <aboud.coding@gmail.com>
2021-10-12 19:12:43 +0300

    lab 12 done

--- src/main/java/com/example/songr/AlbumController.java
+++ src/main/java/com/example/songr/AlbumController.java
@@ -1,7 +1,9 @@
 package com.example.songr;
 
-//import com.example.songr.repositories.SongrRepository;
+import com.example.songr.Album;
+import com.example.songr.SongrRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 //import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,21 +11,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.view.RedirectView;
-
+@Controller
 public class AlbumController {
     @Autowired
-    SongrRepo songrRepo;
+    SongrRepository songrRepository;
 
-    @GetMapping("/albums")
+    @GetMapping("/addAlbum")
     public String getAlbumList(Model model) {
-        model.addAttribute("Albums", songrRepo.findAll());
-        return "albums";
+        model.addAttribute("Albums", songrRepository.findAll());
+        return "addAlbum";
     }
 
-    @PostMapping("/addAlbums")
+    @PostMapping("/addAlbum")
     public RedirectView addAlbum(@ModelAttribute Album album, Model model) {
         model.addAttribute("Album", album);
-        songrRepo.save(album);
-        return new RedirectView("albums");
+        songrRepository.save(album);
+        return new RedirectView("/albums");
     }
 }
--- src/main/java/com/example/songr/SongrRepository.java
+++ src/main/java/com/example/songr/SongrRepository.java
@@ -2,6 +2,6 @@ package com.example.songr;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface SongrRepo extends CrudRepository<Album, Long> {
+public interface SongrRepository extends CrudRepository<Album, Long> {
 
 }
